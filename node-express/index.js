9a0a3a572e3ae1d63af8e42580fcb312c52e4747
kevinwalsh23 <kevinwalsh23@gmail.com>
2019-02-12 17:33:09 -0500

    assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -6,17 +6,25 @@ const port = 3000;
 const app = express();
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
-app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promos', promoRouter);
+app.use('/leaders', leaderRouter);
+
 app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
-app.use('/dishes', dishRouter);
+
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
